f3e2f2d3231d44bafe1afbebb92e9c8bbe8337a0
shaoxiaodan <sandays_cn@163.com>
2019-09-03 10:49:28 +0800

    updated VO file of User

--- src/com/vo/UserVO.java
+++ src/com/vo/UserVO.java
@@ -2,4 +2,32 @@ package com.vo;
 
 public class UserVO {
 
+	private int id;
+	private String name;
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
